dc744c4de31300b0ab317abf1082fbe2b183b78e
Nick <nicholas.kazan1@gmail.com>
2019-08-21 16:08:05 -0400

    Updates

---
+++ precache-manifest.7ebd34982d71eda68e033a4d6015e55d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7844c4c253aa681682206fc9dc977bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b51c21292aa9eed0ad",
+    "url": "/static/css/main.85ce5487.chunk.css"
+  },
+  {
+    "revision": "8c88eadfbef32272e2e4",
+    "url": "/static/js/2.6d349381.chunk.js"
+  },
+  {
+    "revision": "51b51c21292aa9eed0ad",
+    "url": "/static/js/main.5d562469.chunk.js"
+  },
+  {
+    "revision": "8171ea1990bb2199d1cd",
+    "url": "/static/js/runtime~main.8b8fb827.js"
+  },
+  {
+    "revision": "8a0202e54a5dec12ead7937a8b422469",
+    "url": "/static/media/white-amazon-logo.8a0202e5.png"
+  }
+]);
