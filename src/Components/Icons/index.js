36efef1a19b465246559778794c73edb638d65e3
Nicholas Kazan <nicholas.kazan1@gmail.com>
2019-08-27 16:05:02 -0400

    Updated contact section to have responsive buttons (#12)

--- src/Components/Icons/index.js
+++ src/Components/Icons/index.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import insta from '../../assets/insta-logo.png';
-import fb from '../../assets/fb-logo.png';
-import linkedin from '../../assets/linkedin-logo.png';
-import github from '../../assets/github-logo.png';
+import InstaLogo from '../../Icons/InstaLogo';
+import FacebookLogo from '../../Icons/FacebookLogo';
+import LinkedInLogo from '../../Icons/LinkedinLogo';
+import GithubLogo from '../../Icons/GithubLogo';
 
 class Icons extends React.Component {
   render() {
     return (
       <StyledIcons>
         <div className="icon-cluster">
-          <a href="https://www.instagram.com/nickkazan/" target="_blank" rel="noopener noreferrer">
-            <img src={insta} className="icon" alt="insta_logo"/>
+          <a href="https://www.instagram.com/nickkazan" className="image-link" target="_blank" rel="noopener noreferrer">
+            <InstaLogo className="icon" alt="insta_logo"/>
           </a>
-          <a href="https://www.facebook.com/nickkazan123" target="_blank" rel="noopener noreferrer">
-            <img src={fb} className="icon" alt="facebook_logo"/>
+          <a href="https://www.facebook.com/nickkazan123" className="image-link" target="_blank" rel="noopener noreferrer">
+            <FacebookLogo className="icon" alt="facebook_logo"/>
           </a>
         </div>
         <div className="icon-cluster">
-          <a href="https://www.linkedin.com/in/nickkazan/" target="_blank" rel="noopener noreferrer">
-            <img src={linkedin} className="icon" alt="linkedin_logo"/>
+          <a href="https://www.linkedin.com/in/nickkazan/" className="image-link" target="_blank" rel="noopener noreferrer">
+            <LinkedInLogo className="icon" alt="linkedin_logo"/>
           </a>
-          <a href="https://github.com/nickkazan" target="_blank" rel="noopener noreferrer">
-            <img src={github} className="icon" alt="github_logo"/>
+          <a href="https://github.com/nickkazan" className="image-link" target="_blank" rel="noopener noreferrer">
+            <GithubLogo className="icon" alt="github_logo"/>
           </a>
         </div>
-
       </StyledIcons>
     )
   }
 }
 
 const StyledIcons = styled.div `
+
+  .image-link {
+    margin: 25px;
+  }
+
   .icon-cluster {
     display: flex;
     flex-direction: row;
@@ -40,10 +44,19 @@ const StyledIcons = styled.div `
   }
 
   .icon {
-    width: calc(50px + 4vw);
-    height: calc(50px + 4vw);
-    margin: 25px;
+    stroke: #fff;
+    fill: none;
+    viewBox: 0 0 10 10;
   }
+
+  .icon:hover {
+    -webkit-transform: scale(1);
+    -moz-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    fill: #119DA4;
+    stroke: #119DA4;
+  }  
 `
 
 export default Icons;
